2063216e9325204162d0540ea19830b9d112102c
Mustakim Patvekar <mustakip@thoughtworks.com>
2020-02-05 16:40:38 +0530

    some correction in list_test.clj

--- test/assignments/lists_test.clj
+++ test/assignments/lists_test.clj
@@ -11,8 +11,8 @@
         [2 3 4] (map' inc [1 2 3]))))
 
   (testing "filter"
-    (testing "single coll"
-      (are [x y]
-        (= x y)
-        '(1 3 5) (filter' odd? [1 2 3 4 5])
-        '(2 4) (filter' even? [1 2 3 4 5])))))
+    (are [x y]
+      (= x y)
+      '(1 3 5) (filter' odd? [1 2 3 4 5])
+      '(2 4) (filter' even? [1 2 3 4 5]))))
+
